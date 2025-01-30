fcf4f0f4016101b942f6d307aac9adff3f978841
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2025-01-30 13:12:04 +0100

    Added tsc

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.scrollHandler = scrollHandler;
+function scrollHandler(a) {
+    console.log("balls balls");
+    return 2;
+}
+window.addEventListener("scroll", scrollHandler);
