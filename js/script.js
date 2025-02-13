1742ec157234675de4bc5d60e6b1d27c6599aae2
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2025-02-13 11:47:32 +0100

    Driver schema changed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.scrollHandler = scrollHandler;
+exports.getDriverById = getDriverById;
+exports.drawDriverCard = drawDriverCard;
 function scrollHandler(a) {
     if (window.scrollY > 75) {
         document.getElementById("navbar").classList.add("bg-black/90", "border-b", "border-white");
@@ -10,5 +12,18 @@ function scrollHandler(a) {
     }
     return 23;
 }
-1;
+var Side;
+(function (Side) {
+    Side["LEFT"] = "left";
+    Side["RIGHT"] = "right";
+})(Side || (Side = {}));
+function getDriverById(id, side) {
+    fetch("../json/drivers.json").then(function (response) { return response.json(); }).then(function (data) {
+        var driver = data.drivers.find(function (i) { return i.driver_id == id; });
+        drawDriverCard(driver, side);
+    });
+}
+function drawDriverCard(data, side) {
+    document.getElementById(side).innerHTML = "\n  <div>\n    <h2 class=\"font-semibold text-red-500\">".concat(data.full_name, "</h2>\n  </div>\n\n  ");
+}
 window.addEventListener("scroll", scrollHandler);
