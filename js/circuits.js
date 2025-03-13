1f8d11ca338a48b46579be443325ebe28ec3cce6
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2025-03-13 12:09:57 +0100

    Coords

--- js/circuits.js
+++ js/circuits.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('./json/circuits.json')
+const getGeo = exports.getGeo;
+function init(){
+    fetch('./json/circuits.json')
     .then(response => response.json())
     .then(circuits => {
       const circuit1Select = document.getElementById('circuit1');
       const circuit2Select = document.getElementById('circuit2');
-
+      
       Object.values(circuits).forEach(circuit => {
         const option1 = document.createElement('option');
         option1.value = circuit.circuit_name;
@@ -16,23 +17,24 @@ document.addEventListener('DOMContentLoaded', () => {
         option2.textContent = circuit.circuit_name;
         circuit2Select.appendChild(option2);
       });
-
+      
       circuit1Select.addEventListener('change', () => {
         displayCircuit(circuit1Select.value, 'left', circuits);
       });
-
+      
       circuit2Select.addEventListener('change', () => {
         displayCircuit(circuit2Select.value, 'right', circuits);
       });
     })
     .catch(error => console.error('Error loading circuits:', error));
-});
-
-function displayCircuit(circuitName, side, circuits) {
-  const circuit = Object.values(circuits).find(c => c.circuit_name === circuitName);
-  if (circuit) {
-    const circuitCard = document.getElementById(side);
-    circuitCard.innerHTML = `
+  };
+  
+  function displayCircuit(circuitName, side, circuits) {
+    const circuit = Object.values(circuits).find(c => c.circuit_name === circuitName);
+    if (circuit) {
+      getGeo(circuit.geo_id, side); 
+      const circuitCard = document.getElementById(side);
+      circuitCard.innerHTML = `
       <h2 class="circuit-name">${circuit.circuit_name}</h2>
       <p class="circuit-location">${circuit.location}</p>
       <p class="circuit-length">Length: ${circuit.length}</p>
@@ -40,6 +42,10 @@ function displayCircuit(circuitName, side, circuits) {
       <p class="circuit-laps">Laps: ${circuit.laps}</p>
       <p class="circuit-race-distance">Race Distance: ${circuit.race_distance}</p>
       <p class="circuit-lap-record">Lap Record: ${circuit.lap_record}</p>
-    `;
-  }
+      `;
+    }
+  
+  
 }
+
+document.addEventListener("DOMContentLoaded", init)
---
+++ js/geo.js
@@ -0,0 +1,57 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getGeo = getGeo;
+var Side;
+(function (Side) {
+    Side[Side["LEFT"] = 0] = "LEFT";
+    Side[Side["RIGHT"] = 1] = "RIGHT";
+})(Side || (Side = {}));
+let e1;
+let e2;
+let map1; 
+let map2;
+document.addEventListener("DOMContentLoaded", ()=> {
+     e1 = document.getElementById("map1");
+     e2 = document.getElementById("map2");
+     map1 = L.map(e1).setView([51.505, -0.09], 6);
+     map2 = L.map(e2).setView([51.505, -0.09], 6);
+     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map1);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map2);
+})
+function getGeo(circuit_id, side) {
+    if (map1 && map2) {
+        fetch(`../json/circuits_geo/${circuit_id}`).then((res) => {
+            res.json().then((data) => {
+                console.log(data.bbox);
+                let vec = [0, 0]
+                if (data.bbox[0] > 100){
+                    vec[0] = data.bbox[0] - 100;
+                }
+                else {
+                    vec[0] = data.bbox[0];
+                }
+                if (data.bbox[1] > 100){
+                    vec[1] = data.bbox[1] 
+                    + 100
+                }
+                else{
+                    vec[1] = data.bbox[1]
+                }
+                if (side === Side.LEFT) {
+                    L.geoJSON(data).addTo(map1);
+                    map1.setView(vec, 8)
+                }
+                else {
+                    L.geoJSON(data).addTo(map1);
+                    map2.setView(vec, 8)
+                }
+            });
+        });
+    }
+}
